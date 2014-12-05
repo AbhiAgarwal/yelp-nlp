b6f1c56784da2ce3c836b91401c6c640779ef86b
Abhi Agarwal <me@abhiagarwal.com>
2014-12-04 21:57:09 -0500

    Adding New York State

---
+++ clean-yelp-state-review.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import argparse, collections, os, csv, sys, locale
+import simplejson as json
+
+if __name__ == '__main__':
+	stateNeeded = 'NY'
+	business_json_file = './yelp/yelp_academic_dataset_business.json'
+
+	businessData = []
+	with open(business_json_file) as f:
+		for line in f:
+			businessData.append(json.loads(line))
+	state = []
+	for i in businessData:
+		key = i['state']
+		if key == stateNeeded:
+			state.append(i)
+	with open('./yelp-' + stateNeeded + '/business.json', 'w') as outfile:
+		json.dump(state, outfile)
