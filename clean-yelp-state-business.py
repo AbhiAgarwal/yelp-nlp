976471a7944ef567c161206148e239f3bc48a737
Abhi Agarwal <me@abhiagarwal.com>
2014-12-04 23:53:00 -0500

    Adding state details

--- clean-yelp-state-business.py
+++ clean-yelp-state-business.py
@@ -1,35 +1,19 @@
-"""
-Cleaning Yelp dataset
-    1. Taking JSON and converting it to CSV
-    2. Removing type, business_id, user_id, date
-
-"""
-
 # -*- coding: utf-8 -*-
 import argparse, collections, os, csv, sys, locale
 import simplejson as json
 
 if __name__ == '__main__':
-	stateNeeded = 'NY'
-	business_json_file = './yelp-' + stateNeeded + '/business.json'
-	review_json_file = './yelp/yelp_academic_dataset_review.json'
+	stateNeeded = 'ON'
+	business_json_file = './yelp/yelp_academic_dataset_business.json'
 
 	businessData = []
 	with open(business_json_file) as f:
 		for line in f:
 			businessData.append(json.loads(line))
-	allIDs = []
-	for i in businessData[0]:
-		allIDs.append(i['business_id'])
-
-	reviewData = []
-	with open(review_json_file) as f:
-		for line in f:
-			reviewData.append(json.loads(line))
-	eachStateReviews = []
-	for i in reviewData:
-		businessID = i['business_id']
-		if businessID in allIDs:
-			eachStateReviews.append(i)
-	with open('./yelp-' + stateNeeded + '/review.json', 'w') as outfile:
-		json.dump(eachStateReviews, outfile)
+	state = []
+	for i in businessData:
+		key = i['state']
+		if key == stateNeeded:
+			state.append(i)
+	with open('./yelp-' + stateNeeded + '/business.json', 'w') as outfile:
+		json.dump(state, outfile)
