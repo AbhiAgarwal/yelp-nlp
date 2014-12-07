f7859330bbeea6071e43668f57c644f7ea57d420
Abhi Agarwal <me@abhiagarwal.com>
2014-12-07 11:30:41 -0500

    Updating yelp-NV files to remove JSON

--- yelp-NV/json-to-csv.py
+++ yelp-NV/json-to-csv.py
@@ -16,22 +16,27 @@ reviewData = reviewData[0]
 f = csv.writer(open(csvFile, "wb+"))
 
 # Write CSV Header, If you dont need that, remove this line
-f.writerow(["latitude", "longitude", "stars", "review_stars", "text", "sentiment", "votes", "review_count", "average_stars", "yelping_since", "fans"])
+f.writerow(["latitude", "longitude", "stars", "review_stars", "text", "sentiment", "review_count", "average_stars", "yelping_since", "fans", "funnyVotes", "usefulVotes", "coolVotes"])
 
 currentCount = 0
 limit = 10000
 
 for x in reviewData:
     if currentCount < limit:
+        funnyVotes = x["votes"]["funny"]
+        usefulVotes = x["votes"]["useful"]
+        coolVotes =  x["votes"]["cool"]
         f.writerow([unicode(x["latitude"]).encode(("utf-8")),
                     unicode(x["longitude"]).encode(("utf-8")),
                     unicode(x["stars"]).encode(("utf-8")),
                     unicode(x["review_stars"]).encode(("utf-8")),
                     unicode(x["text"]).encode(("utf-8")),
                     unicode(x["sentiment"]).encode(("utf-8")),
-                    unicode(x["votes"]).encode(("utf-8")),
                     unicode(x["review_count"]).encode(("utf-8")),
                     unicode(x["average_stars"]).encode(("utf-8")),
                     unicode(x["yelping_since"]).encode(("utf-8")),
-                    unicode(x["fans"]).encode(("utf-8"))])
+                    unicode(x["fans"]).encode(("utf-8")),
+                    unicode(funnyVotes).encode(("utf-8")),
+                    unicode(usefulVotes).encode(("utf-8")),
+                    unicode(coolVotes).encode(("utf-8"))])
         currentCount += 1
